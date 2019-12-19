6aceaa7ca9893e8355416b5ba5d0864a35ec4f53
zlik <ozarichnyi@gmail.com>
2019-12-18 19:29:56 -0800

    removed

--- component---src-pages-resume-js-a0c42c8d76a7abf6a8ad.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{F2CN:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),r=a("Bl7J"),c=a("vrFN");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(c.a,{title:"Page two"}),l.a.createElement("h1",null,"My resume"),l.a.createElement("p",null,"Welcome to my resume"),l.a.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-resume-js-a0c42c8d76a7abf6a8ad.js.map
